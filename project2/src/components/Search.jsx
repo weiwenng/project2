df9b829de596067b6a00725e41461ee01d1ed3e4
weiwen <weiwen.97@hotmail.com>
2022-07-24 02:15:45 +0800

    remove background

--- project2/src/components/Search.jsx
+++ project2/src/components/Search.jsx
@@ -3,20 +3,20 @@ import { useEffect, useState } from "react";
 import BasicCard from "./BasicCard";
 import { makeStyles } from "@mui/styles"
 
-const useStyles = makeStyles({
-  root: {
-    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    background: 'white',
-    border: 0,
-    borderRadius: 3,
-    color: 'white',
-    width: 0,
-    height: 55,
-    // padding: '0 0px',
-  }
-    }
-)
+// const useStyles = makeStyles({
+//   root: {
+//     // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+//     // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+//     background: 'white',
+//     border: 0,
+//     borderRadius: 3,
+//     color: 'white',
+//     width: 0,
+//     height: 55,
+//     // padding: '0 0px',
+//   }
+//     }
+// )
 
 const Search = ({ favs, handleFavs, removeFavs }) => {
   // should be an array of strings
@@ -26,7 +26,7 @@ const Search = ({ favs, handleFavs, removeFavs }) => {
   const [types, setTypes] = useState();
   const [search, setSearch] = useState();
   const API_KEY = import.meta.env.VITE_API_KEY;
-  const styleClass = useStyles();
+  // const styleClass = useStyles();
   useEffect(() => {
     fetch(`https://tih-api.stb.gov.sg/content/v1/datasets?apikey=${API_KEY}`)
       .then((response) => response.json())
@@ -36,8 +36,7 @@ const Search = ({ favs, handleFavs, removeFavs }) => {
         return (
           value &&
           fetch(
-            `https://tih-api.stb.gov.sg/content/v1/${
-              value.includes("_") ? value.replace("_", "-") : value
+            `https://tih-api.stb.gov.sg/content/v1/${value.includes("_") ? value.replace("_", "-") : value
             }/types?language=en&apikey=${API_KEY}`
           )
             .then((response) => response.json())
@@ -56,7 +55,7 @@ const Search = ({ favs, handleFavs, removeFavs }) => {
         <Autocomplete
           disablePortal
           id="categories"
-          className={styleClass.root}
+          // className={styleClass.root}
           variant="filled"
           options={test}
           sx={{ width: 300, margin: "auto" }}
@@ -83,14 +82,15 @@ const Search = ({ favs, handleFavs, removeFavs }) => {
                 setValueCat(newValue);
               }}
               renderInput={(params) => (
-                <TextField value={types} {...params} className={styleClass.root} label="Types" />
+                <TextField value={types} {...params}
+                  //  className={styleClass.root} 
+                  label="Types" />
               )}
             />
             <Button
               onClick={() => {
                 fetch(
-                  `https://tih-api.stb.gov.sg/content/v1/${
-                    value.includes("_") ? value.replace("_", "-") : value
+                  `https://tih-api.stb.gov.sg/content/v1/${value.includes("_") ? value.replace("_", "-") : value
                   }/search?keyword=${types}&language=en&apikey=GnloiEy5Obcp0Z0dDikvxUf6krTNEQtF`
                 )
                   .then((response) => response.json())
